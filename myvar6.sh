d8dbff78c45ac82b213059c38a8bdc91f97a4852
someotb <llyryk934@gmail.com>
2025-11-17 14:30:31 +0700

    Update myvar6.sh

--- myvar6.sh
+++ myvar6.sh
@@ -1,28 +1,33 @@
-while getopts ":fm:" opt; do
-    case $opt in
-        f)
-            echo "Введите команду: "
-            read cmd
-            echo "Выполняю команду: $cmd"
-            eval "$cmd"
-            ;;
-        m)
-            filename="$NAME"
-            if [ -f "$filename" ]; then
-                echo "Содержимое файла '$filename': "
-                cat "$filename"
-            else
-                echo "Файл '$filename' - не существует"
-            fi
-            ;;
-        \?)
-            echo "Недопустимая опция: -$NAME"
-            echo "Использование: $0 [-f] [-m filename]"
-            exit 1
-            ;;
-        :)
-            echo "Опция -m требует аргумент."
-            exit 1
-            ;;
-    esac
-done
+
+if [ "$#" -gt 0 ]; then
+    while getopts ":fm:" opt; do
+        case $opt in
+            f)
+                echo "Введите команду: "
+                read cmd
+                echo "Выполняю команду: $cmd"
+                eval "$cmd"
+                ;;
+            m)
+                filename="$OPTARG"
+                if [ -f "$filename" ]; then
+                    echo "Содержимое файла '$filename': "
+                    cat "$filename"
+                else
+                    echo "Файл '$filename' - не существует"
+                fi
+                ;;
+            \?)
+                echo "Недопустимая опция: -$OPTARG"
+                echo "Использование: $0 [-f] [-m filename]"
+                exit 1
+                ;;
+            :)
+                echo "Опция -m требует аргумент."
+                exit 1
+                ;;
+        esac
+    done
+else
+    echo "Используйте аргументы"   
+fi
