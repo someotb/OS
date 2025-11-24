88a02a837374952dd1a758b8da297f02a01b9e74
someotb <llyryk934@gmail.com>
2025-11-24 14:39:49 +0700

    feat: add choice to out process to foreground

--- src/myvar6.sh
+++ src/myvar6.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -m 
+
 # Долгоиграющий процесс — поиск "namespace" в .h файлах
-sleep 1000 &
+sleep 10 &
 PID=$!
 echo "Долгоиграющий процесс запущен. PID=$PID"
 
@@ -9,9 +11,10 @@ while true; do
     clear
     echo "Меню управления процессом PID=$PID:"
     echo "1) Остановить процесс"
-    echo "2) Продолжить выполнение"
-    echo "3) Завершить процесс"
-    echo "4) Завершить принудительно"
+    echo "2) Продолжить выполнение на заднем плане (bg)"
+    echo "3) Перевести на передний план (fg)"
+    echo "4) Завершить процесс"
+    echo "5) Завершить принудительно"
     echo "0) Выйти из меню"
     echo
     read -p "Выберите опцию: " choice
@@ -23,6 +26,7 @@ while true; do
                 echo "Процесс $PID приостановлен"
             else
                 echo "Процесс $PID уже завершён"
+                break
             fi
             read -p "Нажмите Enter, чтобы продолжить..."
             ;;
@@ -32,13 +36,16 @@ while true; do
                 echo "Процесс $PID продолжен"
             else
                 echo "Процесс $PID уже завершён"
+                break
             fi
             read -p "Нажмите Enter, чтобы продолжить..."
             ;;
         3)
             if kill -0 $PID 2>/dev/null; then
-                kill $PID
-                echo "Процесс $PID завершён"
+                echo "Перевод процесса на передний план. Скрипт приостановится до завершения процесса или перевода его обратно в фон."
+                fg %1
+                echo "Процесс завершился"
+                break
             else
                 echo "Процесс $PID уже завершён"
             fi
@@ -46,12 +53,21 @@ while true; do
             ;;
         4)
             if kill -0 $PID 2>/dev/null; then
+                kill $PID
+                echo "Процесс $PID завершён"
+            else
+                echo "Процесс $PID уже завершён"
+            fi
+            break
+            ;;
+        5)
+            if kill -0 $PID 2>/dev/null; then
                 kill -9 $PID
                 echo "Процесс $PID завершён принудительно"
             else
                 echo "Процесс $PID уже завершён"
             fi
-            read -p "Нажмите Enter, чтобы продолжить..."
+            break
             ;;
         0)
             echo "Выход"
